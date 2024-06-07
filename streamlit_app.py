83e4949f030e57217543ad35384cb226caa36730
Snezroamia <1221302721@student.mmu.edu.my>
2024-06-07 21:19:07 +0800

    Finished model training and development

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,88 @@
-import altair as alt
-import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve, auc
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import streamlit as st
+from joblib import load
+from lr_model import X, scaler, y_test, X_test_scaled
+plt.ion()
 
-"""
-# Welcome to Streamlit!
+import warnings
+warnings.filterwarnings("ignore")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("Online Payments Fraud Detection")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if 'model' not in st.session_state:
+    st.session_state['model'] = None
+if 'encoder' not in st.session_state:
+    st.session_state['encoder'] = None
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+def main():
+    # Load the model into session state
+    if st.button('Load Logistic Regression Model'):
+        st.session_state['model'] = load('lr_model.joblib')
+        st.session_state['encoder'] = load('lr_encoder.joblib')
+        print("Logistic Regression Model loaded successfully!")
+        
+    if st.button('Load Decision Tree Model'):
+        st.session_state['model'] = load('dt_model.joblib')
+        st.session_state['encoder'] = load('dt_encoder.joblib')
+        print("Decision Tree Model loaded successfully!")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    if st.session_state['model'] is not None:
+        st.write('Model Loaded')
+        
+        # Create separate input sections for each feature
+        step = st.number_input('Enter Step:', value=0)
+        type = st.selectbox('Select Type:', ["CASH_IN", "CASH_OUT", "DEBIT", "PAYMENT", "TRANSFER"])
+        amount = st.number_input('Enter Amount:', value=0)
+        oldbalanceOrg = st.number_input('Enter Old Balance Orig:', value=0)
+        newbalanceOrig = st.number_input('Enter New Balance Orig:', value=0)
+        oldbalanceDest = st.number_input('Enter Old Balance Dest:', value=0)
+        newbalanceDest = st.number_input('Enter New Balance Dest:', value=0)
+        isFlaggedFraud = st.toggle("Is the transaction flagged as fraud?", value=0)
+        
+        # Create a button to make predictions
+        if st.button('Make Prediction'):
+            # Create a DataFrame with the input values
+            new_data_df = pd.DataFrame({'step': [step], 'type': [type], 'amount': [amount], 'oldbalanceOrg': [oldbalanceOrg], 'newbalanceOrig': [newbalanceOrig], 'oldbalanceDest': [oldbalanceDest], 'newbalanceDest': [newbalanceDest], 'isFlaggedFraud' : [isFlaggedFraud]})
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+            # Encode categorical values
+            new_data_df['type'] = st.session_state['encoder'].transform(new_data_df['type'])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+            # Scale the data
+            new_data_scaled = scaler.transform(new_data_df)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+            # Make prediction
+            prediction = st.session_state['model'].predict(new_data_scaled)
+            print("Prediction:", prediction)
+            st.write('Prediction:', prediction)
+            
+            # Display model metrics
+            st.write('Model Metrics:')
+            st.write('Accuracy:', accuracy_score(y_test, st.session_state['model'].predict(X_test_scaled)))
+            st.write('Precision:', precision_score(y_test,st.session_state['model'].predict(X_test_scaled)))
+            st.write('Recall:', recall_score(y_test, st.session_state['model'].predict(X_test_scaled)))
+            st.write('F1 Score:', f1_score(y_test, st.session_state['model'].predict(X_test_scaled)))
+            st.write('ROC-AUC Score:', roc_auc_score(y_test, st.session_state['model'].predict_proba(X_test_scaled)[:, 1]))
+
+            # Display confusion matrix
+            st.write('Confusion Matrix:')
+            st.write(confusion_matrix(y_test, st.session_state['model'].predict(X_test_scaled)))
+
+            # Display ROC-AUC curve
+            st.write('ROC-AUC Curve:')
+            y_pred_proba = st.session_state['model'].predict_proba(X_test_scaled)[:, 1]
+            fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
+            roc_auc = auc(fpr, tpr)
+            plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.title('ROC-AUC Curve')
+            plt.legend(loc='best')
+            st.pyplot(plt)
+            
+            
+if __name__ == '__main__':
+    main()
